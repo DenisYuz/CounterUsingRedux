e4313f57ff7c50cc23424619b8a11dc922a2d1f9
Denis Yuzhelevsky <denislevsky@gmail.com>
2019-05-04 20:47:28 +0300

    creating counter app using redux

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+import { statement } from '@babel/template';
+
+const initialState = {
+    count: 3
+};
+
+const reducer = (state = initialState, action) => {
+    console.log('reducer running', action);
+    switch (action.type) {
+        case 'INCREMENT':
+            // return Object.assign({}, state, { count: state.count + 1 })
+            return { ...state, count: state.count + 1 };
+        default:
+            return state;
+    }
+
+}
+
+const store = createStore(reducer);
+
+export default store;
